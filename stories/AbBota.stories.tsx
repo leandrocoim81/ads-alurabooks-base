53ca3abcf1b3ca44140aa38c45fc0a5d4af4e15f
Leandro Coimbra <leandro.coimbra@gmail.com>
2024-03-22 17:57:10 -0300

    alterando AbBotaStories

--- stories/AbBota.stories.tsx
+++ stories/AbBota.stories.tsx
@@ -9,6 +9,27 @@ export default
     component: AbBotao
 } as ComponentMeta<typeof AbBotao>
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />
+interface AbBotaoProps
+{
+    tipo?: 'primario' | 'secundario'
+    texto?: string
+    onClick?: () => void
+}
+
+const Template: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args} />
 
 export const Primario = Template.bind({})
+
+Primario.args =
+{
+    tipo: 'primario',
+    texto: 'AbBotao Primário'
+} as AbBotaoProps
+
+export const Secundario = Template.bind({})
+
+Secundario.args =
+{
+    tipo: 'secundario', 
+    texto: 'AbBotao Secundario'
+} as AbBotaoProps
